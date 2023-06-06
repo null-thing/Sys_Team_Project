854af7b9e63e9d25f3c233adbcd71717679857f5
ssbnn <sbzzang41@gmail.com>
2023-06-07 00:41:16 +0900

    climain.c & checklist update

--- ssb_cli/climain.c
+++ ssb_cli/climain.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <conio.h>
 
 int start_door_num(){
     int num;
@@ -37,7 +38,27 @@ void current_door_cli(const char* datapath, int dnum) { // if accept by one door
     fclose(file);
 }
 
-int history_reader(int n){ // when you request {n}door history, open history data and show data
+int history_reader(const char* datapath, int dnum){ // when you request {n}door history, open history data and show data
+    FILE* file = fopen(datapath, "r");
+    char quitcommand[100];
+    system("cls");
+    if (file == NULL) {// if {n} door history is not found
+        printf(">> Door %i's history is not found\n", dnum);
+        return 0;
+    }
+    printf("Door %d history \n", dnum);
+    printf("time                         light sensor    door sensor   door open\n");
+    char buffer[1000] = {0, };
+    fread(buffer, 1, 1000, file);
+
+    printf("%s", buffer);
+    fclose(file);
+    printf("\n\n\n----------------------------------------------------\n");
+    printf("if you want to quit, please insert Quit\n$command line >> ");
+    scanf("%s", quitcommand);
+    if (strcmp(quitcommand, "Quit") == 0){
+        return 1;
+    }
     return 0;
 }
 
@@ -49,7 +70,7 @@ int main() {
     system("cls");
     int totaldoornum = start_door_num();
     printf("Total door number: %d\n", totaldoornum);
-    sleep(2);
+    sleep(1);
 
     // -------------------current door session-------------------
     for(; ;){
@@ -67,14 +88,42 @@ int main() {
 
             current_door_cli(prepath, i+1);
         }
-
-        // ------------------time print line-----------------------
         time_t currentTime = time(NULL);
         char* timeString = ctime(&currentTime);
-        printf("current time: %s \n\n", timeString);
-        printf("-------------------------------------------------------");
-        sleep(5); // reset cycle, not yet sellected
-        // ------------------command input line
+        printf("\ncurrent time: %s \n\n", timeString);
+        printf("-------------------------------------------------------\n");
+        printf("$command line >> ");
+            // you should make history write code
+
+    // -------------------------- history print line ----------------------
+        // you should update hitory.txt while history section is until open
+        if (kbhit()){
+            char input[100];
+            fgets(input, sizeof(input),stdin);
+            strtok(input, "-");
+            char doorcheck[100];
+            int history_door_num;
+            
+            sscanf(input, "%s %d", doorcheck, &history_door_num);
+            printf("input pharase: %s \ndoor number: %d\n", doorcheck, history_door_num);
+
+            if (strcmp(doorcheck, "door_history") == 0){
+                for(; ;){
+                    int quitsin = history_reader("C:\\Users\\ssbnn\\Desktop\\C++\\SP_2023\\Sys_Team_Project\\ssb_cli\\testfolder\\history1.txt", history_door_num); // path rebuild needed
+                    if (quitsin){
+                        break;
+                    }
+                }
+
+                sleep(5);
+            }
+            else{
+                system("cls");
+                printf("\n command error: please insert 'door_history {n}'\n");
+            }
+        }
+
+        sleep(2); // reset cycle, not yet sellected
 
     }
 
