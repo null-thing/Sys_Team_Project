c840ce6788783d0318b3d60d04940415b852a80f
HojeongKim <cabi930@gmail.com>
2023-06-07 00:19:44 +0900

    edit test client

--- PC_to_PI_hub/comm_module_client.c
+++ PC_to_PI_hub/comm_module_client.c
@@ -5,29 +5,31 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+int PORT_NUMBER = 8010; // set to desired port number
+char* SERVER_IP_ADDRESS = "127.0.0.1";
 
-int main(void) {
-    int PORT_NUMBER = 700; // set to desired port number
-    char* SERVER_IP_ADDRESS = "192.168.0.4";
+
+int main(void) {    
+    int client_socket, client_size;
+    struct sockaddr_in server_addr, client_addr;
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
     
-    int socket_desc , client_sock, client_size;
-    struct sockaddr_in server_addr ,client_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    if (client_sock == -1){
+    if (client_socket == -1){
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
+    memset(&server_addr, 0, sizeof(server_addr));
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT_NUMBER);
 
-
     if (inet_pton(AF_INET, SERVER_IP_ADDRESS, &server_addr.sin_addr) <= 0) {
-    perror("Invalid server address");
-    exit(EXIT_FAILURE);
+        perror("Invalid server address");
+        exit(EXIT_FAILURE);
     }
-
-    if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
@@ -36,10 +38,9 @@ int main(void) {
 
     // 예를 들어,
     // use IO function such as read and write,
-    /*
     char message[] = "hello server! from client.";
-    write(client_sock, message, sizeof(message)); //5번
-    */
+    send(client_socket, message, sizeof(message),0);
 
-    close(client_sock);
+    close(client_socket);
+    return 0;
 }
